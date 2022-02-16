20586ea294552fa7b04297dc38f8c20f391b13af
ahmedsoliman8 <mitomahmoud2022@gmail.com>
2022-02-16 16:44:57 +0200

    The Basics

---
+++ resources/views/post.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<head>
+    <title>My Blog</title>
+    <link rel="stylesheet" href="/app.css"></link>
+</head>
+<body>
+
+<article>
+    <h1><?= $post->title ?></h1>
+    <div><?= $post->body ?></div>
+</article>
+
+<a href="/">Go Back</a>
+
+
+</body>
+
---
+++ resources/views/posts.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<head>
+    <title>My Blog</title>
+    <link rel="stylesheet" href="/app.css"></link>
+</head>
+<body>
+
+<?php foreach ($posts as $post) : ?>
+<article>
+    <h1><a href="posts/<?=$post->slug?>"><?= $post->title ?></a></h1>
+    <div><?= $post->excerpt ?></div>
+</article>
+
+<?php endforeach; ?>
+
+
+
+
+
+
+</body>
+
